426f6f42ae851b5e4747b003b6303774a0e64fd8
Nanda Irjayandi <nanda.irjayandi@gmail.com>
2026-03-06 23:26:48 +1100

    update Dockerfile testing suite

--- test.sh
+++ test.sh
@@ -1,1 +1,19 @@
-docker compose run --remove-orphans --build --rm dotfiles-alpine sh -c "./bootstrap.sh && exec zsh"
+#!/usr/bin/env zsh
+
+# docker compose run --remove-orphans --build --rm dotfiles-alpine sh -c "./bootstrap.sh && exec zsh"
+
+execute() {
+    docker compose run --remove-orphans --build --rm $1 sh -c "./bootstrap.sh && exec zsh"
+}
+
+execute_alpine() {
+    execute "dotfiles-alpine"
+}
+
+execute_ubuntu_non_root() {
+    execute "dotfiles-ubuntu-non-root"
+}
+
+execute_ubuntu_root() {
+    execute "dotfiles-ubuntu-root"
+}
