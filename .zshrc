c0ef2ee1b8b84389b07a7868b65b863d2eab8fc1
Nanda Irjayandi <nanda.irjayandi@gmail.com>
2026-03-16 18:28:40 +1100

    remove deleted files

--- .zshrc
+++
@@ -1,8 +0,0 @@
-export ZSH="$HOME/.oh-my-zsh"
-
-plugins=(
-  git
-  zsh-syntax-highlighting
-  zsh-autosuggestions
-  zsh-vi-mode
-)
--- zsh/rc.d/setopt.zsh
+++
@@ -1,8 +0,0 @@
-# History
-
-setopt HIST_IGNORE_ALL_DUPS # remove all earlier duplicate lines
-setopt APPEND_HISTORY # history appends to existing file
-setopt SHARE_HISTORY # import new commands from the history file also in other zsh-session
-setopt EXTENDED_HISTORY # save each commands beginning timestamp and the duration to the history file
-setopt HIST_REDUCE_BLANKS # trim multiple insgnificant blanks in history
-setopt HIST_IGNORE_SPACE # don’t store lines starting with space
